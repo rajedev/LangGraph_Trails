f84228239d49b36f68606af21561410e9b966c2d
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-07-13 17:41:52 +0530

    snapshot -- resume behaviour

--- Practice_Light.py
+++ Practice_Light.py
@@ -24,6 +24,9 @@ llm_with_config = llm.with_config(configurable={
 })
 checkpointer = InMemorySaver()
 
+exception_action = True
+
+
 # manual conversation w/o checkpointer and config - checks
 # convo = []
 
@@ -33,7 +36,11 @@ class MsgState(TypedDict):
 
 def chat_here(state: MsgState):
     u_msg = state['messages']
-    if u_msg[-1].content == "exception":
+    log = u_msg[-1].content.split("-")
+    # print(f"log={log}")
+    u_msg = log[-1]
+    global exception_action
+    if exception_action == True and "exception" in log:
         raise Exception("Err Checker -- Manual Invoke")
     # convo.extend(u_msg)
     # print(u_msg)
@@ -56,13 +63,20 @@ def workflow_graph():
 
 def execute_graph():
     graph = workflow_graph()
+    global exception_action
     while True:
         usr_input = str(input("User: "))
+        us = usr_input.split("-")
+        # print(us)
+        if "exception " in us:
+            exception_action = True
+        if usr_input == "er":
+            exception_action = False
         if usr_input == "thread":
-            #conversation = checkpointer.list(None)
+            # conversation = checkpointer.list(None)
             conversation = checkpointer.list(config=config)
             for co in conversation:
-                #print(co[1]['channel_values'])
+                # print(co[1]['channel_values'])
                 print(co[0])
             continue
         if usr_input == "snap":
@@ -71,17 +85,36 @@ def execute_graph():
             continue
         if usr_input in ["/bye", "exit", "quit"]:
             break
-        initial_state = {
+        convo_input = None if usr_input == "er" else {
             "messages": [HumanMessage(content=usr_input)]
             # "messages": convo + [HumanMessage(content=usr_input)]
         }
         try:
-            result = graph.invoke(input=initial_state, config=config)
+            result = graph.invoke(input=convo_input, config=config)
             # result = graph.invoke(input=initial_state)
             print(f"AI: {result["messages"][-1].content}")
         except Exception as e:
-            print(f"\n\n Except: {e}")
+            print(f"\nException: {e}\n")
 
 
 if __name__ == "__main__":
     execute_graph()
+
+# Conversation for references - Exception raised & AI response from the last leaved convo on "er" to clear the error
+"""
+
+User: hi
+AI: Hello! How can I assist you today? 😊
+User: exception-this is rajesh, works as developer, suggest me with 20 words on carrer advice
+
+Exception: Err Checker -- Manual Invoke
+
+User: thread
+....
+User: snap
+....
+User: er
+AI: Continuously learn new languages, contribute to open source, master algorithms, network actively, prioritize code quality, seek mentorship, and embrace feedback.
+User: /bye
+
+"""
